aedefad3e37ced1398a9e1d32c950bfe6db8d32f
Mark <markperry.uk@gmail.com>
2023-03-04 17:14:38 +0000

    Add viewbinding output to UI

--- app/src/main/java/uk/co/digitaljeeves/retrofitexample/IAlbumService.kt
+++ app/src/main/java/uk/co/digitaljeeves/retrofitexample/IAlbumService.kt
@@ -4,7 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface IAlbumService {
-    //base = https://jsonplaceholder.typicode.com
     @GET("/albums")
     suspend fun getAlbums() : Response<Albums>
 }
--- app/src/main/java/uk/co/digitaljeeves/retrofitexample/MainActivity.kt
+++ app/src/main/java/uk/co/digitaljeeves/retrofitexample/MainActivity.kt
@@ -2,10 +2,38 @@ package uk.co.digitaljeeves.retrofitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import retrofit2.Response
+import retrofit2.Retrofit
+import uk.co.digitaljeeves.retrofitexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val retService: IAlbumService = RetrofitInstance.getRetrofitInstance().create(IAlbumService::class.java)
+        val responseLiveData:LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null){
+                while (albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    val result =
+                        " Album title : ${albumsItem.title}\n"+
+                        " Album id : ${albumsItem.id}\n"+
+                        " user id : ${albumsItem.userId}\n\n\n"
+                    Log.i("MyTag", result)
+                    binding.tvTitle.append(result)
+                }
+            }
+        })
     }
 }
