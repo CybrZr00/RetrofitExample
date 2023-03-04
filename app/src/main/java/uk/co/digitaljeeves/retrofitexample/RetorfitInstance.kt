9dbd1afa6a891b3bbe0d2063d5ce6307720c73e4
Mark <markperry.uk@gmail.com>
2023-03-04 19:34:37 +0000

    Add timeouts

--- app/src/main/java/uk/co/digitaljeeves/retrofitexample/RetorfitInstance.kt
+++ app/src/main/java/uk/co/digitaljeeves/retrofitexample/RetorfitInstance.kt
@@ -5,16 +5,20 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
 
     companion object{
-        const val BASE_URL = "https://jsonplaceholder.typicode.com"
+        private const val BASE_URL = "https://jsonplaceholder.typicode.com"
         private val interceptor = HttpLoggingInterceptor().apply {
             this.level = HttpLoggingInterceptor.Level.BODY
         }
         private val client = OkHttpClient.Builder().apply {
             this.addInterceptor(interceptor)
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(20, TimeUnit.SECONDS)
+                .writeTimeout(20, TimeUnit.SECONDS)
         }.build()
         fun getRetrofitInstance():Retrofit{
             return Retrofit.Builder()
